67970cda42a6a271f7891915c58db49184b0899a
Jagadeeswar Venkatasubbu <jagadeeswarvenkatasubbu@gmail.com>
2021-06-01 20:06:04 +1000

    update type safety

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import React, { FC } from "react";
 
 export default function Layout({ children }) {
     return (
